9379505c3a367f90decdc9dae309454bd2d73a42
ankit singh <ankitsingh12346@gmail.com>
2022-10-06 01:19:05 +0530

    Added Movie Adapter

--- app/src/main/java/com/example/mvvm_clean_architecture1/presentation/movie/MovieActivity.kt
+++ app/src/main/java/com/example/mvvm_clean_architecture1/presentation/movie/MovieActivity.kt
@@ -3,26 +3,56 @@ package com.example.mvvm_clean_architecture1.presentation.movie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.transition.Visibility
 import com.example.mvvm_clean_architecture1.R
 import com.example.mvvm_clean_architecture1.databinding.ActivityMovieBinding
 import com.example.mvvm_clean_architecture1.presentation.di.Injector
+import javax.inject.Inject
 
 class MovieActivity : AppCompatActivity() {
     lateinit var binding: ActivityMovieBinding
+
+    @Inject
     lateinit var viewModelFactory: MovieViewModelFactory
     lateinit var viewModel: MovieViewModel
+    private lateinit var adapter: MovieAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this@MovieActivity, R.layout.activity_movie)
         (application as Injector).createMovieSubComponent().inject(this)
         viewModel = ViewModelProvider(this, viewModelFactory)
             .get(MovieViewModel::class.java)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        binding.movieRecyclerView.layoutManager = LinearLayoutManager(this@MovieActivity)
+        adapter = MovieAdapter()
+        binding.movieRecyclerView.adapter = adapter
+        displayPopularMovies()
+    }
+
+    private fun displayPopularMovies() {
+        binding.movieProgressBar.visibility = View.VISIBLE
         val responseLiveData = viewModel.getMovies()
         responseLiveData.observe(this, Observer {
-            Log.i("MYTAG" , it.toString())
+            Log.i("MYTAG", it.toString())
+            if (it != null) {
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.movieProgressBar.visibility = View.GONE
+            } else {
+                binding.movieProgressBar.visibility = View.GONE
+                Toast.makeText(this, "No Data found on device", Toast.LENGTH_LONG).show()
+            }
         })
     }
 }
